17ff3b6f1015ae34ee598b59355b1ca01d9cda35
Artur Kudyrko <kudyrkoartur@gmail.com>
2024-12-12 16:26:33 +0200

    new file:   FirebaseConfig.ts 	modified:   hooks/useFontsLoad.ts 	modified:   package-lock.json 	modified:   package.json 	modified:   pages/SignIn/index.tsx 	modified:   pages/SignUp/index.tsx 	modified:   styles/styles.ts

--- hooks/useFontsLoad.ts
+++ hooks/useFontsLoad.ts
@@ -1,9 +1,11 @@
-import { Poppins_400Regular, useFonts } from "@expo-google-fonts/poppins";
+import { Poppins_400Regular, Poppins_500Medium, Poppins_700Bold, useFonts } from "@expo-google-fonts/poppins";
 
 
 export default function useFontsLoad() {
     const [fontsLoad] = useFonts({
       Poppins_400Regular,
+      Poppins_500Medium,
+      Poppins_700Bold
     })
 
     return fontsLoad;
--- pages/SignIn/index.tsx
+++ pages/SignIn/index.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import useFontsLoad from '../../hooks/useFontsLoad';
-import { ActivityIndicator, Button, StatusBar, Text, View } from 'react-native';
+import { ActivityIndicator, TextInput, Button, StatusBar, Text, View, Pressable } from 'react-native';
 import styles from '../../styles/styles';
+import { firebase_auth } from '../../FirebaseConfig';
 
 export default function SignIn({navigation}: any) {
     const fontsLoad = useFontsLoad()
+    const [email, onChangeEmail] = useState('')
+    const [password, onChangePassword] = useState('')
+    const [loading, setLoading] = useState(false)
+    const auth = firebase_auth;
 
     if(!fontsLoad) {
       return <View>
@@ -13,8 +18,29 @@ export default function SignIn({navigation}: any) {
     } else {
       return (
         <View style={styles.container}>
-         <Text style = {{fontFamily: 'Poppins_400Regular', color: '#fff'}}>Open up App.tsx to start working on your app!</Text>
-         <Button title='Click' onPress={() => navigation.navigate('signup')}/>
+          <Text style = {styles.title}>Sign In</Text>
+          <TextInput placeholder='Email' placeholderTextColor='#fff' value={email} style = {styles.input} onChangeText={onChangeEmail}/>
+          <TextInput
+          style={styles.input}
+          onChangeText={onChangePassword}
+          value={password}
+          placeholder="Password"
+          placeholderTextColor='#fff'
+          keyboardType="numeric"
+          />
+
+          <View style={styles.buttonContainer}>
+            <Pressable style={styles.button}>
+             <Text style={styles.buttonText}>Sign In</Text>
+            </Pressable>
+
+            <View style = {styles.buttonContainerText}>
+              <Text style = {styles.buttonTextButtom}>Don’t have an account?</Text>
+              <Text style = {styles.buttonTextButtomBold} onPress={() => navigation.navigate('signup')}>Sign Up</Text>
+            </View>
+            
+          </View>
+          
         </View>
       );
     }
